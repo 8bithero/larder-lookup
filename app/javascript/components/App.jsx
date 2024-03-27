6b12e15ab68a85dd5fe58fcdad72e8e713f90d1e
Paris Kemanes <pkemanes@gmail.com>
2024-03-27 16:08:26 +0000

    Created endpoint and fetching data

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>You clicked {count} times!</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+  const [recipes, setRecipes] = useState([]);
+
+  const loadRecipes = () => {
+    const url = "/api/v1/recipes/index";
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((res) => setRecipes(res))
+      .catch((err) => message.error('Error: ' + err));
+  };
+
+  useEffect(() => {
+      loadRecipes();
+  }, []);
+
+  const allRecipes = recipes.map((recipe, index) => (
+    <div key={index} className="">
+      <p className="card-title">{recipe.title}</p>
     </div>
+  ));
+
+  const noRecipe = (
+    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+      <h4>
+        No recipes yet.
+      </h4>
+    </div>
+  );
+
+  return (
+    <>
+      <div>
+        <p>You clicked {count} times!</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+      <hr />
+      <div>
+      {recipes.length > 0 ? allRecipes : noRecipe}
+      </div>
+    </>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+    end
+  end
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
